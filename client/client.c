330fd70ded96b2873c10761d6aea15960417bf4e
Preston Mueller <skierjunk@gmail.com>
2015-10-02 15:49:28 -0400

    client: added data link layer frame structure

--- client/client.c
+++ client/client.c
@@ -6,6 +6,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef struct frameData {
+
+	char seqNum[2];
+	char *payload; //somewhere between 1 and 130 bytes
+
+	char errorDetect[2];
+	char eop; //end-of-packet byte
+
+} frame;
+
+typedef struct frameACK {
+
+	char seqNum[2];
+	char errorDetect[2];
+
+} frameACK;
+
 int main(int argc, char** argv) {
 
 	if(argc != 4) {
