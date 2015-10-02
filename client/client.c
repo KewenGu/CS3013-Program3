a329e6372147aefa54be7ad34106f2d15ec12bbb
Preston Mueller <skierjunk@gmail.com>
2015-10-01 21:10:53 -0400

    Added barebones client, checking for correct argument number

---
+++ client/client.c
@@ -0,0 +1,21 @@
+#include <string.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+
+	if(argc != 4) {
+
+		printf("Usage: ./client <servermachine> <id> <num_photos>\n");
+		exit(1);
+
+	}
+
+	//Pointer to socket structure that ends up filled in by gethostbyname
+  	struct hostent *servHost;
+
+}
