b4ec047b6af348c7caa6593726acd22bf953d96b
KewenGu <kewen.gu@gmail.com>
2015-10-10 14:33:38 -0400

    fixed some compling errors 	new file:   client/client.c

---
+++ github/client/client.c
@@ -0,0 +1,264 @@
+#include <string.h>
+#include <stdio.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "client.h"
+
+#define PACKET_SIZE 256
+#define FRAME_PAYLOAD_SIZE 130
+
+#define END_OF_PHOTO_YES ((char)4)   // end of transmission
+#define END_OF_PHOTO_NO ((char)3)    // end of text
+
+#define END_OF_PACKET_YES ((char)4)  // end of transmission
+#define END_OF_PACKET_NO ((char)3)   // end of text
+
+unsigned short seq_num = 0;
+
+int main(int argc, char** argv) {
+
+	if(argc != 4) {
+
+		printf("Usage: ./client <servermachine> <id> <num_photos>\n");
+		exit(1);
+
+	}
+
+	int clientID = atoi(argv[2]);
+	int num_photos = atoi(argv[3]);
+  	int i, j, k;
+
+	//Pointer to socket structure that ends up filled in by gethostbyname
+  	struct hostent *servHost;
+  	
+  	unsigned short port = WELLKNOWNPORT;
+  	servHost = gethostbyname(argv[1]);
+
+  	int sock = physical_Establish(servHost, port);
+
+  	int total_size = 0;
+  	int sizes[num_photos];
+
+  	//Packet memory allocation. First make space, then we can do the math for the packets easier
+  	for(i = 0; i < num_photos; i++) {
+
+  		char filename[255];
+  		sprintf(filename, "photo%d%d.jpg", clientID, i);
+
+  		FILE *file;
+  		file = fopen(filename, "rb");
+  		if(file == NULL) {
+  			fprintf(stderr, "%s couldn't be found!\n", filename);
+  			exit(1);
+  		}
+
+  		fseek(file, 0, SEEK_END);
+  		total_size += ftell(file);
+  		sizes[i] = ftell(file);
+
+  		fclose(file);
+  	}
+
+  	int num_packets = (total_size / PACKET_SIZE) + (total_size % PACKET_SIZE > 0 ? 1 : 0);
+
+  	//Take in the jpg data
+  	Packet *packets = (Packet *)malloc(num_packets * sizeof(Packet));
+
+  	int current_packet = 0;
+  	int current_position = 0;
+  	int totalBytesLoaded = 0;
+
+  	for(i = 0; i < num_photos; i++) {
+
+  		char filename[255];
+  		sprintf(filename, "photo%d%d.jpg", clientID, i);
+      // Open the JPEG file for reading
+  		FILE *file;
+  		file = fopen(filename, "rb");
+  		if(file == NULL) {
+  			fprintf(stderr, "%s couldn't be found!\n", filename);
+  			exit(1);
+  		}
+
+  		int bytesLoaded = sizes[i];
+
+  		while(bytesLoaded > 0) {
+        // Read from JPEG file one byte at a time
+  			fread(packets[current_packet].data+current_position, 1, 1, file);
+
+  			current_position++;
+  			bytesLoaded--;
+        totalBytesLoaded++;
+        // Start filling a new packet after PACKET_SIZE bytes read
+  			if(current_position == PACKET_SIZE) {
+  				current_position = 0;
+          packets[current_packet].endOfPhoto = END_OF_PHOTO_NO; // Indicate not end-of-photo
+  				current_packet++;
+  			}
+  		}
+
+      packets[current_packet].endOfPhoto = END_OF_PHOTO_YES; // Indicate end-of-photo
+      
+      datalink_Layer(&packets[current_packet], sock);
+
+
+
+      current_packet++;
+
+  	}
+
+
+}
+
+
+void DieWithError(char *errorMsg)
+{
+  perror(errorMsg);
+  exit(1);
+}
+
+
+int physical_Establish(struct hostent* host, unsigned short port) {
+
+  printf("Connecting to host on port %d\n", port);
+
+	struct sockaddr_in serverAddress;
+  int sock;
+
+	  //Reset the struct
+  	memset(&serverAddress, 0, sizeof(serverAddress));
+  	serverAddress.sin_family = AF_INET;
+
+  	//Copy the address from the gethostbyname struct into struct
+  	memcpy(&serverAddress.sin_addr.s_addr, host->h_addr_list[0], host->h_length);
+
+  	//Convert the provided port to network byte order and assign to struct
+  	serverAddress.sin_port = htons(port);
+
+	if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    DieWithError("sock() failed");
+
+	if(connect(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0) 
+    DieWithError("connect() failed");
+
+  	return sock;
+
+}
+
+// Put the payload into the frame
+void datalink_Layer(Packet *p, int sock)
+{
+  // First, initialize the frame
+  int packetSize = sizeof(*p);
+  int num_frames = (packetSize / FRAME_PAYLOAD_SIZE) + (packetSize % FRAME_PAYLOAD_SIZE > 0 ? 1 : 0);
+  Frame *frames = (Frame *)malloc(num_frames * sizeof(Frame));
+  int i, count = 0;
+  int current_frame = 0;
+  int bytesFramed = 0;
+  int totalBytesFramed = 0;
+
+  // Copy the packet into the frame payload
+  while(totalBytesFramed < packetSize)
+  {
+    bytesFramed = 0;
+    for(i = 0; i < FRAME_PAYLOAD_SIZE; i++)
+    {
+      printf("iteration %d of %d\n", i, FRAME_PAYLOAD_SIZE);
+      printf("count is %d and current_frame is %d\n", count, current_frame);
+
+      frames[current_frame].payload = malloc(FRAME_PAYLOAD_SIZE * sizeof(unsigned char));
+
+      frames[current_frame].payload[i] = p->data[count];
+      bytesFramed++;
+      count++;
+      // If reach the end-of-photo specifier
+      if(frames[current_frame].payload[i] == END_OF_PHOTO_YES || frames[current_frame].payload[i] == END_OF_PHOTO_NO)
+      {  
+        frames[current_frame].endOfPacket = END_OF_PACKET_YES;
+        break;
+      }
+    }
+
+    frames[current_frame].seqNum[0] = seq_num & 0x00ff;
+    frames[current_frame].seqNum[1] = seq_num & 0xff00;
+
+    seq_num++;
+
+    frames[current_frame].endOfPacket = END_OF_PACKET_NO;
+
+    unsigned char *error_handling_result = error_handling(&frames[current_frame], bytesFramed);
+
+    frames[current_frame].errorDetect[0] = error_handling_result[0];
+    frames[current_frame].errorDetect[1] = error_handling_result[1];
+
+    physical_Send(sock, &frames[current_frame], bytesFramed+5, sizeof(Frame));
+
+    current_frame++;
+  }
+}
+
+void physical_Send(int sock, Frame* buffer, int length, int frameSize) {
+
+  fd_set fileDescriptorSet;
+  FD_ZERO(&fileDescriptorSet);
+  FD_SET(sock, &fileDescriptorSet);
+
+  char *newBuffer = malloc(1000 * sizeof(unsigned char));
+  printf("physical_Send: length is %d\n", length);
+
+  if(send(sock, newBuffer, length, 0) != frameSize) {
+      DieWithError("send() error");
+    }
+
+    struct timeval timer_length;
+    timer_length.tv_sec = 3;
+    timer_length.tv_usec = 0;
+
+    //Frame timer
+    select(sock + 1, &fileDescriptorSet, NULL, NULL, &timer_length);
+
+    if(timer_length.tv_sec == 0 && timer_length.tv_usec == 0) {
+      //Timeout hit
+      //Frame not sent
+
+      //Using a goto to attempt to send the frame again. No hate! :)
+      //goto resend;
+    }
+
+    //There's data to receive
+
+    char incomingData[400];
+    int receivedCount = recv(sock, incomingData, 400, 0);
+    printf("Received %d bytes from server\n", receivedCount);
+
+}
+
+/* Function generates the error detection bytes
+    how this work?
+        suppose the frame in hex representation is "00 01 02 03 04 05 06 07... [2 error detection bytes]"
+        then, error detection bytes = 00^02^04^06... + 01^03^05^07...  (^ is the operation of XOR, + is the operation of concatenation)
+*/
+unsigned char* error_handling(Frame* t, int size)
+{
+  int i;
+  unsigned char* result = malloc(2 * sizeof(unsigned char));
+
+  for (i = 0; i < (size - 2); i += 2) {
+
+    //result[0] = (unsigned char *)t[i] ^ result[0];
+  }
+
+  for (i = 1; i < (size - 2); i += 2) {
+
+    //result[1] = (unsigned char *)t[i] ^ result[1];
+  }
+
+  return result;
+}
+
+
---
+++ github/client/client.h
@@ -0,0 +1,36 @@
+
+#define WELLKNOWNPORT 5280
+
+typedef struct frameData {
+
+	char seqNum[2];
+	unsigned char *payload; //somewhere between 1 and 130 bytes
+	char endOfPacket; // end-of-packet byte should be after the payload/packet
+	char errorDetect[2];
+	
+} Frame;
+
+typedef struct frameACK {
+
+	char seqNum[2];
+	char errorDetect[2];
+
+} FrameACK;
+
+
+typedef struct packet {
+
+	char data[256];
+	char endOfPhoto;
+
+} Packet;
+
+
+
+void DieWithError(char *errorMsg);
+
+int physical_Establish(struct hostent* host, unsigned short port);
+void physical_Send(int sock, Frame* buffer, int length, int frameSize);
+
+void datalink_Layer(Packet *p, int sock);
+unsigned char* error_handling(Frame* t, int size);
